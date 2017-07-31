42267210c1badbb9ac81f01f23405770bbc8ab10
Randy Lai <randy.cs.lai@gmail.com>
2017-07-30 20:02:19 -0400

    use pyte 0.7-dev

--- terminal_emulator.py
+++ terminal_emulator.py
@@ -32,11 +32,9 @@ class PyteTerminalEmulator():
 
     def prev_page(self):
         self._screen.prev_page()
-        self._screen.ensure_screen_width()
 
     def next_page(self):
         self._screen.next_page()
-        self._screen.ensure_screen_width()
 
     def dirty_lines(self):
         dirty_lines = {}
@@ -76,19 +74,7 @@ History = namedtuple("History", "top bottom ratio size position")
 Margins = namedtuple("Margins", "top bottom")
 
 
-class CustomHistoryScreen(pyte.DiffScreen):
-    """
-    Custom history screen customized for this plugin. Basically a copy of the
-    standard pyte history screen but with some optimizations.
-    """
-    def __init__(self, columns, lines, history, ratio):
-        self.history = History(deque(maxlen=history // 2),
-                               deque(maxlen=history),
-                               float(ratio),
-                               history,
-                               history)
-
-        super(CustomHistoryScreen, self).__init__(columns, lines)
+class CustomHistoryScreen(pyte.HistoryScreen):
 
     def scroll_to_bottom(self):
         """
@@ -96,150 +82,6 @@ class CustomHistoryScreen(pyte.DiffScreen):
         """
         while self.history.position < self.history.size:
             self.next_page()
-
-    def ensure_screen_width(self):
-        """
-        Ensure all lines on a screen have proper width columns. Extra characters
-        are truncated, missing characters are filled with whitespace.
-        """
-        for idx, line in enumerate(self.buffer):
-            if len(line) > self.columns:
-                self.buffer[idx] = line[:self.columns]
-            elif len(line) < self.columns:
-                self.buffer[idx] = line + take(self.columns - len(line),
-                                               self.default_line)
-
-        # If we're at the bottom of the history buffer and `DECTCEM`
-        # mode is set -- show the cursor.
-        self.cursor.hidden = not (
-            abs(self.history.position - self.history.size) < self.lines and
-            modes.DECTCEM in self.mode
-        )
-
-    def reset_history(self):
-        self.history.top.clear()
-        self.history.bottom.clear()
-        self.history = self.history._replace(position=self.history.size)
-
-    def reset(self):
-        """
-        Overloaded to reset screen history state: history position
-        is reset to bottom of both queues;  queues themselves are
-        emptied.
-        """
-        super(CustomHistoryScreen, self).reset()
-        self.reset_history()
-
-    def erase_in_display(self, how=0):
-        """
-        Overloaded to reset history state
-        """
-        super(CustomHistoryScreen, self).erase_in_display(how)
-
-        if how == 3:
-            self.reset_history()
-
-    def index(self):
-        """
-        Overloaded to update top history with the removed lines
-        """
-        top, bottom = self.margins
-
-        if self.cursor.y == bottom:
-            self.history.top.append(self.buffer[top])
-
-        super(CustomHistoryScreen, self).index()
-
-    def reverse_index(self):
-        """
-        Overloaded to update bottom history with the removed lines
-        """
-        top, bottom = self.margins
-
-        if self.cursor.y == top:
-            self.history.bottom.append(self.buffer[bottom])
-
-        super(CustomHistoryScreen, self).reverse_index()
-
-    def prev_page(self):
-        """
-        Move the screen page up through the history buffer
-        """
-        if self.history.position > self.lines and self.history.top:
-            mid = min(len(self.history.top),
-                      int(math.ceil(self.lines * self.history.ratio)))
-
-            self.history.bottom.extendleft(reversed(self.buffer[-mid:]))
-            self.history = self.history \
-                ._replace(position=self.history.position - self.lines)
-
-            self.buffer[:] = list(reversed([
-                self.history.top.pop() for _ in range(mid)
-            ])) + self.buffer[:-mid]
-
-            self.dirty = set(range(self.lines))
-
-    def next_page(self):
-        """
-        Move the screen page down through the history buffer
-        """
-        if self.history.position < self.history.size and self.history.bottom:
-            mid = min(len(self.history.bottom),
-                      int(math.ceil(self.lines * self.history.ratio)))
-
-            self.history.top.extend(self.buffer[:mid])
-            self.history = self.history \
-                ._replace(position=self.history.position + self.lines)
-
-            self.buffer[:] = self.buffer[mid:] + [
-                self.history.bottom.popleft() for _ in range(mid)
-            ]
-
-            self.dirty = set(range(self.lines))
-
-    def resize(self, lines=None, columns=None):
-        lines = lines or self.lines
-        columns = columns or self.columns
-
-        # First resize the lines:
-        line_diff = self.lines - lines
-
-        # a) if the current display size is less than the requested
-        #    size, add lines to the bottom.
-        if line_diff < 0:
-            self.buffer.extend(take(self.columns, self.default_line)
-                               for _ in range(line_diff, 0))
-        # b) if the current display size is greater than requested
-        #    size, take lines off the top.
-        elif line_diff > 0:
-            # JW tweak - if we only have spaces in the bottom of the screen
-            # remove those lines instead
-            disp = self.display[-line_diff:]
-            contents = "".join(disp)
-            if contents.isspace():
-                self.buffer[-line_diff:] = ()
-            else:
-                self.buffer[:line_diff] = ()
-
-        # Then resize the columns:
-        col_diff = self.columns - columns
-
-        # a) if the current display size is less than the requested
-        #    size, expand each line to the new size.
-        if col_diff < 0:
-            for y in range(lines):
-                self.buffer[y].extend(take(abs(col_diff), self.default_line))
-        # b) if the current display size is greater than requested
-        #    size, trim each line from the right to the new size.
-        elif col_diff > 0:
-            for line in self.buffer:
-                del line[columns:]
-
-        self.lines, self.columns = lines, columns
-        self.margins = Margins(0, self.lines - 1)
-
-        # JW tweak - move cursor upwards if its out of bounds do not reset it
-        self.ensure_bounds(use_margins=True)
 
 
 def take(n, iterable):
@@ -284,7 +126,7 @@ def convert_pyte_buffer_to_colormap(buffer, lines):
         field_length = 0
 
         char_index = 0
-        for char in line:
+        for char in line.values():
             # Default bg is black
             if char.bg is "default":
                 bg = "black"
