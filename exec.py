dd9b163d01a0dc37a81e6af41b06c25da12f0ad6
Raoul Wols <raoulwols@gmail.com>
2017-08-04 11:04:27 +0200

    Add comments to exec.py

--- exec.py
+++ exec.py
@@ -7,12 +7,20 @@ class TerminalViewExec(sublime_plugin.WindowCommand):
     _init_args = {}
 
     def run(self, **kwargs):
+        # Get the title for the view.
         name = kwargs.get("name", "Executable")
+
+        # Custom environment variables are ignored for now. LinuxPty should be
+        # able to handle that in the future.
         env = kwargs.get("env", {})
         env = os.environ.copy().update(env)
+
+        # Get the command that we'll invoke.
         cmd = kwargs.get("cmd", [])
         if not cmd:
             cmd = [kwargs.get("shell_cmd", "")]
+        
+        # Get the cwd.
         working_dir = kwargs.get("working_dir")
         if not working_dir:
             view = self.window.active_view()
@@ -22,9 +30,12 @@ class TerminalViewExec(sublime_plugin.WindowCommand):
                 working_dir = env.get("HOME", "")
                 if not working_dir:
                     working_dir = "/"
+        
+        # If there's init args, get those.
         args = kwargs.get("args", "")
         invocation = " ".join(cmd)
         if not args:
+            # Otherwise, prompt the user for init args.
             self.name = name
             self.invocation = invocation
             self.working_dir = working_dir
